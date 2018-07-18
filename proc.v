1f29272e72a55c7cd7c56b45357ba037be48be6c
Guilherme Pereira <guilherme27.gp@gmail.com>
2018-07-18 15:35:05 -0300

    Construindo o multiplexador, finalizando ligações entre entradas e saídas e implementando o reset

--- proc.v
+++ proc.v
@@ -5,25 +5,28 @@ module proc (DIN, Resetn, Clock, Run, Done, BusWires);
 	output [8:0] BusWires;
 
 	//Definição de parametros para melhor controle dos bits
-	parameter T0 = 2'b00, T1 = 2'b01, T2 = 2'b10, T3 = 2'b11, DINout = 4'b1000, Gout = 4'b1001, MV = 2'b00, MVI = 2'b01, ADD = 2'b10, SUB = 2'b11;
+	parameter T0 = 2'b00, T1 = 2'b01, T2 = 2'b10, T3 = 2'b11,
+		MV = 2'b00, MVI = 2'b01, ADD = 2'b10, SUB = 2'b11;
+		DINout_Ctrl = 4'b1000, Gout_Ctrl = 4'b1001, DefaultMulti = 4'b1111;
 
 	//Fios para:	WRX = saída dos registradores de 0 a 7
-	wire [8:0] WR0, [8:0] WR1, [8:0] WR2, [8:0] WR3, [8:0] WR4, [8:0] WR5, [8:0] WR6, [8:0] WR7;
+	wire [8:0] WR0, [8:0] WR1, [8:0] WR2, [8:0] WR3,
+		 [8:0] WR4, [8:0] WR5, [8:0] WR6, [8:0] WR7;
 
 	/*Barramentos para controle de dados do processador:
 		BusFSMReg = controle de acesso aos registradores
 		BusFSMMulti = controle de acesso ao multiplexador
 		BusAout = saída do registrador A
 		BusGout = saída do registrador G
-		BusIRout = saída do registrador IR
+		BusIRout = saída do registrador BusIRout
 		BusUout = saída do registrador U
 	*/
-	wire [7:0] BusFSMReg, [3:0] BusFSMMulti, [8:0] BusAout, [8:0] BusGout, [8:0] BusIRout, [8:0] BusUout;
+	wire [7:0] BusFSMReg, [3:0] BusFSMMulti, [8:0] BusAout,
+		 [8:0] BusGout, [8:0] BusIRout, [8:0] BusUout;
+
 	wire [7:0] WXR, [7:0] WYR;
 
-	/*assign Rx = IR[5:3];
-		assign Ry = IR[2:0];
-	*/
+	wire WAin, WGin, WIRin;
 
 	assign BusAout = Aout;
 	assign BusGout = Gout;
@@ -39,99 +42,127 @@ module proc (DIN, Resetn, Clock, Run, Done, BusWires);
 	assign WR6 = R6;
 	assign WR7 = R7;
 
+	assign WAin = Ain;
+	assign WGin = Gin;
+	assign WIRin = IRin;
+
 	assign WXR = Xreg;
 	assign WYR = Yreg;
 
-	dec3to8 decX (IR[5:3], 1'b1, Xreg);
-	dec3to8 decY (IR[2:0], 1'b1, Yreg);
+	dec3to8 decX (BusIRout[5:3], EnableDec, Xreg);
+	dec3to8 decY (BusIRout[2:0], EnableDec, Yreg);
 
-	regn reg_0 (BusWires, Rin[0], Clock, R0);
-	regn reg_1 (BusWires, Rin[1], Clock, R1);
-	regn reg_2 (BusWires, Rin[2], Clock, R2);
-	regn reg_3 (BusWires, Rin[3], Clock, R3);
-	regn reg_4 (BusWires, Rin[4], Clock, R4);
-	regn reg_5 (BusWires, Rin[5], Clock, R5);
-	regn reg_6 (BusWires, Rin[6], Clock, R6);
-	regn reg_7 (BusWires, Rin[7], Clock, R7);
+	regn reg_0 (BusWires, BusFSMReg[0], Clock, R0);
+	regn reg_1 (BusWires, BusFSMReg[1], Clock, R1);
+	regn reg_2 (BusWires, BusFSMReg[2], Clock, R2);
+	regn reg_3 (BusWires, BusFSMReg[3], Clock, R3);
+	regn reg_4 (BusWires, BusFSMReg[4], Clock, R4);
+	regn reg_5 (BusWires, BusFSMReg[5], Clock, R5);
+	regn reg_6 (BusWires, BusFSMReg[6], Clock, R6);
+	regn reg_7 (BusWires, BusFSMReg[7], Clock, R7);
 
-	regn reg_A (BusWires, Ain, Clock, Aout);
-	regn reg_G (BusWires, Gin, Clock, Gout);
+	regn reg_A (BusWires, WAin, Clock, Aout);
+	regn reg_G (BusWires, WGin, Clock, Gout);
 
 	regn reg_IR (BusWires[8:6], IRin, Clock, IRout);
 
-	ula addSub (BusAout, BusWires, IR[8:6], Uout);
+	ula addSub (BusAout, BusWires, BusIRout[8:6], Uout);
+
+	multiplexador multiplex (WR0, WR1, WR2, WR3, WR4, WR5, WR6, WR7,
+							 DIN, BusGout, BusFSMMulti, BusWires);
 
 	// Controle das saídas da FSM
-	always @(posedge Clock or negedge Resetn or Tstep_Q or I or Xreg or Yreg) begin
-		//. . . especifique os valores iniciais
-		case (Tstep_Q)
-			T0: // Armazene DIN no registrador IR no passo 0
-			begin
-				if (!Run)
-					Tstep_Q = T0;
-
-				else
-					IRin = 1'b1;
-					Tstep_Q = T1;
-			end
-
-			T1: // Defina os sinais do passo 1
+	always @(posedge Clock or negedge Resetn or Tstep_Q or BusIRout or WXR or WYR)
+		begin
+			//. . . especifique os valores iniciais
+			if (!resetn)
 				begin
-					IRin = 1'b0;
-					case (IR[8:6])
-						MV:
-							BusFSMMulti = IR[2:0];
-							BusFSMReg = Xreg;
-							done = 1'b1;
-
-						MVI:
-							BusFSMMulti = DINout;
-							BusFSMReg = Xreg;
-							done = 1'b1;
-
-						ADD:
-							BusFSMMulti = IR[5:3];
-							Ain = 1'b1;
-							TStep_Q = T2;
-
-						SUB:
-							BusFSMMulti = IR[5:3];
-							Ain = 1'b1;
-							TStep_Q = T2;
-
-					endcase
+					Tstep_Q = T0;
+					WIRin = 1'b0;
+					WAin = 1'b0;
+					WGin = 1'b0;
+					BusFSMMulti = DefaultMulti;
+					EnableDec = 1'b0;
+					Done = 1'b1;
 				end
 
-			T2: // Defina os sinais do passo 2
-				case (IR[8:6]) //--------------- I0 e I1 não aparecem pois não terminam de ser execultadas no T1
-					ADD:
-						BusFSMMulti = IR[2:0];
-						Gin = 1'b1;
-						TStep_Q = T3;
+			else
+			case (Tstep_Q)
+				T0: // Armazene DIN no registrador IR no passo 0
+				begin
+					if (!Run)
+						Tstep_Q = T0;
 
-					SUB:
-						BusFSMMulti = IR[2:0];
-						Gin = 1'b1;
-						TStep_Q = T3;
+					else
+						Done = 1'b0;
+						WIRin = 1'b1;
+						EnableDec = 1'b1;
+						Tstep_Q = T1;
+				end
 
-				endcase
+				T1: // Defina os sinais do passo 1
+					begin
+						WIRin = 1'b0;
+						case (BusIRout[8:6])
+							MV:
+								BusFSMMulti = BusIRout[2:0];
+								BusFSMReg = WXR;
+								Done = 1'b1;
+								Tstep_Q = T0;
 
-			T3: // Defina os sinais do passo 3
-				case (IR[8:6]) //--------------- I0 e I1 não aparecem pois não terminam de ser execultadas no T1
-					ADD:
-						BusFSMMulti = Gout;
-						BusFSMReg = Xreg;
-						done = 1'b1;
+							MVI:
+								BusFSMMulti = DINout_Ctrl;
+								BusFSMReg = WXR;
+								Done = 1'b1;
+								Tstep_Q = T0;
 
-					SUB:
-						BusFSMMulti = Gout;
-						BusFSMReg = Xreg;
-						done = 1'b1;
+							ADD:
+								BusFSMMulti = BusIRout[5:3];
+								WAin = 1'b1;
+								TStep_Q = T2;
 
-				endcase
-		endcase
-	end
-endmodule
+							SUB:
+								BusFSMMulti = BusIRout[5:3];
+								WAin = 1'b1;
+								TStep_Q = T2;
+
+						endcase
+					end
+
+				T2: // Defina os sinais do passo 2
+					case (BusIRout[8:6]) //--------------- I0 e I1 não aparecem pois não terminam de ser execultadas no T1
+						ADD:
+							BusFSMMulti = BusIRout[2:0];
+							Gin = 1'b1;
+							TStep_Q = T3;
+
+						SUB:
+							BusFSMMulti = BusIRout[2:0];
+							Gin = 1'b1;
+							TStep_Q = T3;
+
+					endcase
+
+				T3: // Defina os sinais do passo 3
+					case (BusIRout[8:6]) //--------------- I0 e I1 não aparecem pois não terminam de ser execultadas no T1
+						ADD:
+							BusFSMMulti = Gout_Ctrl;
+							BusFSMReg = WXR;
+							Done = 1'b1;
+							Tstep_Q = T0;
+
+						SUB:
+							BusFSMMulti = Gout_Ctrl;
+							BusFSMReg = WXR;
+							Done = 1'b1;
+							Tstep_Q = T0;
+
+					endcase
+				default:
+					Tstep_Q = T0;
+			endcase
+		end
+endmodule // proc
 
 module dec3to8(W, En, Y);
 	input [2:0] W;
@@ -184,3 +215,30 @@ module ula(a, b, sel, Q);
 		endcase
 	end
 endmodule
+
+module multiplexador (R0, R1, R2, R3, R4, R5, R6, R7, DIN, G, CTRL, Q);
+	input [8:0]R0, [8:0]R1, [8:0]R2, [8:0]R3, [8:0]R4, [8:0]R5,
+		  [8:0]R6, [8:0]R7, [8:0]DIN, [8:0]G, [3:0]CTRL;
+
+	output [8:0]Q;
+
+	always @(*)
+		begin
+			case (CTRL)
+				4'd0: Q = R0;
+				4'd1: Q = R1;
+				4'd2: Q = R2;
+				4'd3: Q = R3;
+				4'd4: Q = R4;
+				4'd5: Q = R5;
+				4'd6: Q = R6;
+				4'd7: Q = R7;
+				4'd8: Q = DIN;
+				4'd9: Q = G;
+
+				default: Q = 9'b0;
+			endcase
+		end
+
+
+endmodule // multiplexador
